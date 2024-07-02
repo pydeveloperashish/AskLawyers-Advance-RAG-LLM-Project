3e59d9f9dc19a0c63483b1dd0718e73fb23c1c6b
akushwaha.ext <akushwaha.ext@acciona.com>
2024-07-02 17:39:50 +0530

    query enhancement feature added

---
+++ src/generate_answers.py
@@ -0,0 +1,36 @@
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv, find_dotenv
+from data_retrieval import get_retriever
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from query_enhancement import get_enhanced_query
+
+llm = ChatOpenAI(api_key = 'sk-proj-u8KWkDd79BwkiPIE7xzQT3BlbkFJUsIhCEzXnQHaTC5NfMO2',
+                 model ="gpt-3.5-turbo-0125")
+
+retriever = get_retriever()
+
+custom_template = """
+You are an helpful assistent of law. Answer query in detail
+
+{context}
+
+{question}
+"""
+
+prompt = PromptTemplate(template=custom_template, input_variables=["context", "question"])
+
+
+def generate_answer(query):
+    enhanced_query = get_enhanced_query(query)
+    print()
+    print("enhanced_query: ", enhanced_query)
+    qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": prompt},
+    )
+    response = qa_chain.invoke(enhanced_query)
+    return response['result']
--- src/main.py
+++ src/main.py
@@ -2,55 +2,22 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
-from data_retrieval import get_retriever
-from dotenv import load_dotenv, find_dotenv
+from generate_answers import generate_answer
 import os 
-
 
 app = FastAPI()
 templates = Jinja2Templates(directory="../templates")
-
 # Mount static files
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 
-
-retriever = get_retriever()
-
-custom_template = """
-You are an helpful assistent of law. Answer query in detail
-
-{context}
-
-{question}
-"""
-
-prompt = PromptTemplate(template=custom_template, input_variables=["context", "question"])
-
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
-
-
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=retriever,
-    return_source_documents=True,
-    chain_type_kwargs={"prompt": prompt},
-)
 
 @app.get("/", response_class=HTMLResponse)
 async def get_data(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 @app.post("/", response_class=HTMLResponse)
 async def post_data(request: Request, Input: str = Form(...)):
-    response = qa_chain.invoke(Input)
-    data = {"message": response['result']}
+    response = generate_answer(query = Input)
+    data = {"message": response}
     return templates.TemplateResponse("index.html", {"request": request, "data": data})
